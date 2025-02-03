9bff7ddaafbe74ab460f00a7c622e1a528481a1c
tellar <gastonstanley17@gmail.com>
2025-02-03 19:34:02 +0300

    First commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className="nav__logo">
+          <img src="" alt="navbar logo images" />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import Homes from './pages/Homes';
+import PostDetail from './pages/PostDetail';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import UserProfile from './pages/UserProfile';
+import AuthorsPosts from './pages/AuthorsPosts';
+import CreatePosts from './pages/CreatePosts';
+import EditPosts from './pages/EditPosts';
+import CategoryPosts from './pages/CategoryPosts';
+import Dashboard from './pages/Dashboard';
+import Logout from './pages/Logout';
+import Authors from './pages/Authors';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <Layout/> ,
+    errorElement: <ErrorPage/>,
+    children: [
+      {index: true, element: <Homes />},
+      {path: "post/:id", element: <PostDetail />},
+      {path: "register", element: <Register />},
+      {path: "login", element: <Login />},
+      {path: "profile:/id", element: <UserProfile />},
+      {path: "authors", element: <Authors />},
+      {path: "create", element: <CreatePosts />},
+      {path: "posts/categories/:category", element: <CategoryPosts />},
+      {path: "posts/users/:id", element: <AuthorsPosts />},
+      {path: "myposts/:id", element: <Dashboard />},
+      {path: "posts/:id/edit", element: <EditPosts />},
+      {path: "logout", element: <Logout />},
+    ]
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
