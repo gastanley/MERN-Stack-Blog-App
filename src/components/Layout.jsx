85a2d6ae9565df4af4f6e5573aada7d674879113
tellar <gastonstanley17@gmail.com>
2025-02-07 11:32:54 +0300

    Second commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,6 +9,9 @@ const Layout = () => {
     <>
         <Header/>
             <Outlet/>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam beatae distinctio quod labore quo voluptatibus vitae adipisci sunt, illo consectetur hic mollitia at, quia asperiores, quisquam ipsam totam molestiae. Amet!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit praesentium modi sequi eligendi velit nesciunt illum voluptatibus facilis optio inventore? Nobis repellendus ipsa ad perspiciatis, debitis eius hic a nesciunt!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ab iusto ad iure sapiente ut sequi, cumque magni placeat accusamus minima illo facilis magnam ipsum nostrum. Alias quaerat beatae officia.
         <Footer/>
     </>
   )
---
+++ src/components/Posts.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+import Thumbnail1 from '../images/image1.jpg'
+import Thumbnail2 from '../images/image2.jpg'
+import Thumbnail3 from '../images/image3.jpg'
+import Thumbnail4 from '../images/image4.jpg'
+
+const DUMMY_POSTS = [
+    {
+        id:'1',
+        thumbnail: Thumbnail1,
+        category: 'education',
+        title: 'This is the title of the very first post on this blog',
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas vitae repellat cupiditate ducimus fugiat. Ad cupiditate eum dolore? Nesciunt voluptatum beatae vel, deserunt quasi iste a! Quae tenetur voluptates magnam?",
+        authorID: 3
+    },
+    {
+        id:'2',
+        thumbnail: Thumbnail2,
+        category: 'science',
+        title: 'This is the title of the very second post on this blog',
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas vitae repellat cupiditate ducimus fugiat. Ad cupiditate eum dolore? Nesciunt voluptatum beatae vel, deserunt quasi iste a! Quae tenetur voluptates magnam?",
+        authorID: 1
+    },
+    {
+        id:'3',
+        thumbnail: Thumbnail3,
+        category: 'weather',
+        title: 'This is the title of the very third post on this blog',
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas vitae repellat cupiditate ducimus fugiat. Ad cupiditate eum dolore? Nesciunt voluptatum beatae vel, deserunt quasi iste a! Quae tenetur voluptates magnam?",
+        authorID: 13
+    },
+    {
+        id:'4',
+        thumbnail: Thumbnail4,
+        category: 'farming',
+        title: 'This is the title of the very fourth post on this blog',
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas vitae repellat cupiditate ducimus fugiat. Ad cupiditate eum dolore? Nesciunt voluptatum beatae vel, deserunt quasi iste a! Quae tenetur voluptates magnam?",
+        authorID: 11
+    }
+]
+
+const Posts = () => {
+    const [posts, setPosts] = useState(DUMMY_POSTS)
+  return (
+    <section className="posts">
+        {
+            // posts.map(()=> <PostItem />)        
+        }
+    </section>
+  )
+}
+
+export default Posts
