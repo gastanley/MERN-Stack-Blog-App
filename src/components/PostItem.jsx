b726e6ed979869070a3df2bcb56cdb2800e6a8bf
tellar <gastonstanley17@gmail.com>
2025-02-09 20:30:17 +0300

    Accueil done

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -17,7 +17,7 @@ const PostItem = ({postID, thumbnail, category, title, description, authorID}) =
             <p>{shortDescription}</p>
             <div className="post__footer">
                 <PostAuthor/>
-                <Link to={`posts/categories/${category}`} className='btn category'>{category}</Link>
+                <Link to={`/posts/categories/${category}`} className='btn category'>{category}</Link>
             </div>
         </div>
     </article>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  // const changeInputHandler = (e) => {
+  //   setUserData({
+  //     return {...prevState, [e.target.name]: e.target.value}
+  //   })
+  // }
+
   return (
-    <div>Register</div>
+    <section className="register">
+      <div className="container">
+        <h2>Sign up</h2>
+        <form action="" className="form register__form">
+          <p className="form__error-message">This is an error message</p>
+          <input type="text" name="name" placeholder="Full name" value={userData.name} />
+          {/* onChange={changeInputHandler}/> */}
+        </form>
+      </div>
+    </section>
   )
 }
 
